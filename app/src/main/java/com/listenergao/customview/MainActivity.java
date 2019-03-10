68fc8cab84e7c36ad937a699fc0ef5609be2f5c5
Listener Gao <gaoyashen@linglongtech.com>
2019-03-10 20:51:15 +0800

    Initial commit

---
+++ app/src/main/java/com/listenergao/customview/MainActivity.java
@@ -0,0 +1,18 @@
+package com.listenergao.customview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * create on 19/03/10
+ *
+ * @author listenergao
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
