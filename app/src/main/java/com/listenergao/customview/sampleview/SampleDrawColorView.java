245a8c93d96cd7b5867938466f568513fb5fab8a
Listener Gao <gaoyashen@linglongtech.com>
2019-03-17 14:29:57 +0800

    add drawColor sample

---
+++ app/src/main/java/com/listenergao/customview/sampleview/SampleDrawColorView.java
@@ -0,0 +1,39 @@
+package com.listenergao.customview.sampleview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * create on 2019/3/17
+ *
+ * @author ListenerGao
+ */
+public class SampleDrawColorView extends View {
+
+
+    public SampleDrawColorView(Context context) {
+        this(context, null);
+    }
+
+    public SampleDrawColorView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public SampleDrawColorView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        // 绘制背景颜色为 #88ff0000
+        canvas.drawColor(Color.parseColor("#88ff0000"));
+
+    }
+}
